6810b10b88a46c521f8cf343a09bbdeac06826fe
Daniel Walton <dan@wishbonemedia.com>
2012-12-14 21:26:27 -0600

    Added flag for command line options

--- main.go
+++ main.go
@@ -5,17 +5,23 @@ import (
 	"fmt"
 	"net/http"
 	"time"
+	"flag"
 	)
 
 func main() {
-	fileserver := http.FileServer(http.Dir("/opt/htdocs"))
+	var port *int = flag.Int("port", 8080, "Port to listen on. Default=8080")
+	var path *string = flag.String("path", "./", "Path to serve files from.  Default=./")
+
+	fileserver := http.FileServer(http.Dir(*path))
 
 	srv := &http.Server{
+		Addr: fmt.Sprintf(":%d", *port),
 		Handler: fileserver,
 		ReadTimeout: 30*time.Second,
 		WriteTimeout: 30*time.Second,
 	}
-	fmt.Printf("Listening on port 80...\n")
+	fmt.Printf("Listening on port %d\n", *port)
+	fmt.Printf("Serving from: %s\n", *path)
 	err := srv.ListenAndServe()
 	if err != nil {
 		panic("ListenAndServe: " + err.Error())
